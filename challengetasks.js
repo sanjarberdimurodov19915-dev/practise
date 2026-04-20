6e708d5b087c239cc43573634c624bbc9a33ad62
Sam <sanjarberdimurodov19915@gmail.com>
2026-04-20 16:46:01 +0900

    feat: training challenge tasks

---
+++ challengetasks.js
@@ -0,0 +1,10 @@
+const animals = ["fox", "horse", "ant", "bird", "lion", "wolf", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog", "cat", "bat", "cock", "cow"];
+
+function findAnimals(animals, letter) {
+    const searchLetter = letter.toLowerCase();
+    return animals.filter(name => searchLetter.includes(name[0].toLowerCase()));
+}
+
+
+const result = findAnimals(animals, 'aw');
+console.log(result);
