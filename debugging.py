08c8bdc9c0d682237922124e3b952e7a363084f7
Sam <sanjarberdimurodov19915@gmail.com>
2026-04-28 18:16:55 +0900

    feat: pip & external packages

--- debugging.py
+++ debugging.py
@@ -3,6 +3,7 @@
     (2) Package Manager & External Package
     (3) Debugging
 '''
+from PIL import Image
 import turtle
 print("====== Python Packages & Core Package ======")
 ''' Python Packages/Modules: Core, File and External '''
@@ -30,3 +31,19 @@ with open("material/message.txt", "r") as your_file:
     print("your_content:", your_content)
 
 print("DONE")
+
+print("======  Package Manager & External Package ======")
+'''
+Package Manager 
+    Python - pip, pipenv
+    NodeJS - npm, yarn
+    PHP    - composer
+    MacOS  - brew
+    Windows - winget
+'''
+# External Package > https://pypi.org/
+
+with Image.open("material/logo.png") as img_obj:
+    resized_img = img_obj.resize((200, 200))
+    resized_img.show()
+    resized_img.save("material/sample.png")
